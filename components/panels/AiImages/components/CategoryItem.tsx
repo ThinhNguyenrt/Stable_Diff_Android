5a1e9b864da1f125ea4ecd195e37e9a708bdd95d
tranchitam123 <tranchitam65a@gmail.com>
2025-03-18 10:57:17 +0700

    fix: update UI for AiImagesPanel component

--- components/panels/AiImages/components/CategoryItem.tsx
+++ components/panels/AiImages/components/CategoryItem.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Image, TouchableOpacity } from "react-native";
-import { List, Text } from "react-native-paper";
+import { Icon, List, Text } from "react-native-paper";
 import { Category } from "@/constants/types";
 
 interface CategoryItemProps {
@@ -14,17 +14,32 @@ const CategoryItem: React.FC<CategoryItemProps> = ({ category }) => {
     <View>
       <TouchableOpacity onPress={() => setExpanded(!expanded)}>
         <View
-          style={{ flexDirection: "row", alignItems: "center", padding: 8 }}
+          style={{
+            flexDirection: "row",
+            flex: 1,
+            alignItems: "center",
+            padding: 8,
+            backgroundColor: "#f0f0f0",
+            borderRadius: 10,
+            marginTop: 10,
+          }}
         >
           <Image
             source={category.image}
-            style={{ width: 40, height: 40, marginRight: 10 }}
+            style={{ width: 40, height: 40, marginRight: 10, flex: 1/4 }}
             resizeMode="contain"
           />
-          <Text>{category.name}</Text>
-          <Text style={{ marginLeft: "auto", color: "gray" }}>
-            {category.size}
-          </Text>
+          <View style={{ rowGap: 10, flexDirection: "row", flex: 3/4 }}>
+            <View style={{ flex: 3/4 }}>
+              <Text style={{ fontWeight: "bold", fontSize: 20, marginBottom: 5, }}>
+                {category.name}
+              </Text>
+              <Text style={{ color: "gray" }}>{category.size}</Text>
+            </View>
+            <View style={{ flex: 1/4 }}>
+              <Icon source="chevron-down" size={40} color="gray" />
+            </View>
+          </View>
         </View>
       </TouchableOpacity>
       {expanded && (
--- components/panels/AiImages/components/ReferenceImageUploader.tsx
+++ components/panels/AiImages/components/ReferenceImageUploader.tsx
@@ -1,19 +1,54 @@
 import React from "react";
-import { View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { View, Button, TouchableOpacity, StyleSheet } from "react-native";
+import { Icon, Text } from "react-native-paper";
 
 const ReferenceImageUploader = () => {
   return (
-    <View style={{ alignItems: "center", marginBottom: 16 }}>
-      <Text variant="titleMedium">Reference Image</Text>
-      <Button mode="outlined" style={{ marginTop: 8 }}>
-        Choose Image
-      </Button>
-      <Text style={{ fontSize: 12, color: "gray", marginTop: 4 }}>
-        Match the content from your own image
+    <View style={{ marginBottom: 16 }}>
+      <Text
+        variant="titleMedium"
+        style={{ marginBottom: 8, fontSize: 25, paddingTop: 20 }}
+      >
+        Reference Image
       </Text>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={{ flex: 1 / 3, justifyContent: "center", alignItems: "center" }}>
+          <Icon source="upload" size={70} color="gray" />
+        </View>
+        <View style={{ flex: 2 / 3 }}>
+          <TouchableOpacity
+            style={styles.button}
+            // onPress={() => alert("Choose Image")}
+          >
+            <Text style={styles.buttonText}>Choose Image</Text>
+          </TouchableOpacity>
+          <Text style={{ fontSize: 13, color: "black", marginTop: 4, marginLeft: 15, marginRight: 15, textAlign: "center" }}>
+            Match the content from your own image
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#e9e9e9", // Màu nền nút
+    borderColor: "black",
+    paddingVertical: 10, // Khoảng cách trên/dưới của nút
+    paddingHorizontal: 20, // Khoảng cách trái/phải của nút
+    borderRadius: 8, // Bo góc cho nút
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 8,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: "black", // Màu chữ
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+});
 
 export default ReferenceImageUploader;
